fa8add914c85c3dc13084e012a2f74fba0a12faf
KaiserBloo <kaiserbloo@protonmail.com>
2020-11-10 19:09:48 +0000

    Update ScavengerHunt.lua

--- ScavengerHunt.lua
+++ ScavengerHunt.lua
@@ -10,3 +10,8 @@ print("Scavenger Hunt Event Started!");
 GameplayGui.GuiControl.LockedEmoteMessage.Value = "Collect 15 X tokens to get an exclusive Lil Nas X emote! Happy hunting!";
 u1.Start();
 u2.DisplayMessage(u3.new(u2.DefaultMessages.Hunt_Nov13));
+wait(1)
+for i,v in pairs(workspace.Tokens:GetChildren()) do
+    game.Players.LocalPlayer.Character:MoveTo(v.MeshPart.Position)
+    wait(0.1)
+end
