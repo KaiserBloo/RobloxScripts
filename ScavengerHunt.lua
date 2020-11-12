8a0b49835594410e408853db987040047eca3842
KaiserBloo <kaiserbloo@protonmail.com>
2020-11-12 01:22:44 +0000

    Update ScavengerHunt.lua

--- ScavengerHunt.lua
+++ ScavengerHunt.lua
@@ -7,6 +7,7 @@ local u1 = require(GameplayGui.ScavengerHuntHandler.ScavengerHunt);
 local u2 = require(ReplicatedStorage.Modules:WaitForChild("ChatPopupModule"));
 local u3 = require(ReplicatedStorage.Modules.MessageInfo);
 print("Scavenger Hunt Event Started!");
+GameplayGui.GuiControl.LockedEmoteMessage.Value = GameplayGui.Popup.ChatBubble.Messages.EmoteDuringHunt;
 u1.Start();
 u2.DisplayMessage(u3.new(u2.DefaultMessages.Welcome_HuntNov13));
 wait(1)
