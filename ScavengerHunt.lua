23bfa5d42d340891eb10b8bbc99ec339ce2f0080
KaiserBloo <kaiserbloo@protonmail.com>
2020-11-12 01:18:47 +0000

    Update ScavengerHunt.lua

--- ScavengerHunt.lua
+++ ScavengerHunt.lua
@@ -7,11 +7,10 @@ local u1 = require(GameplayGui.ScavengerHuntHandler.ScavengerHunt);
 local u2 = require(ReplicatedStorage.Modules:WaitForChild("ChatPopupModule"));
 local u3 = require(ReplicatedStorage.Modules.MessageInfo);
 print("Scavenger Hunt Event Started!");
-GameplayGui.GuiControl.LockedEmoteMessage.Value = "Collect 15 X tokens to get an exclusive Lil Nas X emote! Happy hunting!";
 u1.Start();
-u2.DisplayMessage(u3.new(u2.DefaultMessages.Hunt_Nov13));
+u2.DisplayMessage(u3.new(u2.DefaultMessages.Welcome_HuntNov13));
 wait(1)
 for i,v in pairs(workspace.Tokens:GetChildren()) do
     game.Players.LocalPlayer.Character:MoveTo(v.MeshPart.Position)
-    wait(0.1)
+    wait(0.25)
 end
